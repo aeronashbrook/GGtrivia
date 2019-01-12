e2f5f9a4a1feb86eadb098d08e82cbb67d7dab09
Spencer Caretta <spencer.caretta@gmail.com>
2019-01-12 13:22:11 -0600

    start to htmlRoutes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,22 +3,47 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
+    db.Questions.findAll({}).then(function(dbQuestions) {
+      res.render("index");
+    });
+  });
+
+  // Load index page
+  app.get("/quiz", function(req, res) {
+    db.Questions.findAll({}).then(function(dbQuestions) {
+      res.render("quiz");
+    });
+  });
+
+  // Load index page
+  app.get("/leaderboard", function(req, res) {
+    db.Leaderboard.findAll({}).then(function(dbLeaderbord) {
+      res.render("leaderboard");
+    });
+  });
+
+  // Load index page
+  app.get("/loser", function(req, res) {
+    db.Leaderboard.findAll({}).then(function(dbLeaderboard) {
+      res.render("loser");
+    });
+  });
+
+  // Load index page
+  app.get("/winner", function(req, res) {
+    db.Example.findAll({}).then(function(dbLeaderboard) {
+      res.render("winner");
     });
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
+  // app.get("/example/:id", function(req, res) {
+  //   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  //     res.render("example", {
+  //       example: dbExample
+  //     });
+  //   });
+  // });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
