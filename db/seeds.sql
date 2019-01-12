f7931b4cb21e5d172d5831e4f6b0242a8ce5669f
Mark Sperle-Weiler <40145728+mdsw81@users.noreply.github.com>
2019-01-12 11:19:14 -0600

    Update seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
 INSERT INTO questions (question, option1, option2, option3, option4, correctAnswer, category)
-VALUES ("What is the tallest building in the world as of 2018?", "Shanghai Tower", "Burj Khalifa", "Kingdom Clock Tower", "One World Trade Center", "Burj Khalifa", "miscellaneous"),
-(),
-(),
-(),
-();
+VALUES ("What is the tallest building in the world as of 2018?", "Shanghai Tower", "Burj Khalifa", "Kingdom Clock Tower", "One World Trade Center", "Burj Khalifa", "Famous Places and Landmarks"),
+("The Colosseum is located in which country?", "Italy", "France", "Norway", "Germany", "Italy", "Famous Places and Landmarks"),
+("The Great Barrier Reef is located in which country?", "Australia", "China", "Taiwan", "South Africa", "Australia", "Famous Places and Landmarks"),
+("In which city is the world's largest medieval cathedral found?", "Paris", "Seville", "Rome", "Athens", "Seville", "Famous Places and Landmarks"),
+("Why was the White House painted white?", "White paint was the cheapest.", "Congress voted on it.", "To cover scars after a fire.", "The American people voted on it.", "To cover scars after a fire.", "Famous Places and Landmarks.");
