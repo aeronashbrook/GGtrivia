1885be9d8af1a7a24a1e2c1662b862a3bc95d23f
Spencer Caretta <spencer.caretta@gmail.com>
2019-01-12 10:42:55 -0600

    added question to seeds.sql file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,6 @@
 INSERT INTO questions (question, option1, option2, option3, option4, correctAnswer, category)
-VALUES ();
+VALUES ("What is the tallest building in the world as of 2018?", "Shanghai Tower", "Burj Khalifa", "Kingdom Clock Tower", "One World Trade Center", "Burj Khalifa", "miscellaneous"),
+(),
+(),
+(),
+();
