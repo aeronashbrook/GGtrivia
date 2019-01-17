0407ba313a6dc3a40a2c3f5124e77d73526904b8
Spencer Caretta <spencer.caretta@gmail.com>
2019-01-16 22:49:42 -0600

    not sure of the changes

--- models/questions.js
+++ models/questions.js
@@ -1,4 +1,5 @@
 module.exports = function(sequelize, DataTypes) {
+<<<<<<< HEAD
   var Questions = sequelize.define("questions", {
     // id: {
     //   type: DataTypes.INTEGER,
@@ -79,4 +80,66 @@ module.exports = function(sequelize, DataTypes) {
     timestamps: false
   });
   return Questions;
+=======
+    var Questions = sequelize.define("questions", {
+        question: {
+            type: DataTypes.STRING,
+            validate: {
+                isAlphanumeric: true,
+                allowNull: false,
+                len: [1, 500]
+            }
+        },
+        option1: {
+            type: DataTypes.STRING,
+            validate: {
+                isAlphanumeric: true,
+                allowNull: false,
+                len: [1,255]
+            }
+        },
+        option2: {
+            type: DataTypes.STRING,
+            validate: {
+                isAlphanumeric: true,
+                allowNull: false,
+                len: [1,255]
+            }
+        },
+        option3: {
+            type: DataTypes.STRING,
+            validate: {
+                isAlphanumeric: true,
+                allowNull: false,
+                len: [1,255]
+            }
+        },
+        option4: {
+            type: DataTypes.STRING,
+            validate: {
+                isAlphanumeric: true,
+                allowNull: false,
+                len: [1,255]
+            }
+        },
+        correctAnswer: {
+            type: DataTypes.STRING,
+            validate: {
+                isAlphanumeric: true,
+                allowNull: false,
+                len: [1,255]
+            }
+        },
+        category: {
+            type: DataTypes.STRING,
+            validate: {
+                isAlphanumeric: true,
+                allowNull: false,
+                len: [1,50]
+            }
+        }
+
+    });
+    return Questions;
+>>>>>>> not sure of the changes
 };
