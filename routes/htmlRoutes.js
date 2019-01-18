4972ae6e9acdef700224526ec7c76dd136d346ca
e-g-jackson <42052471+e-g-jackson@users.noreply.github.com>
2019-01-18 06:12:59 -0600

    Got app.js working, also moved around files.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,30 +4,33 @@ var path = require('path');
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/index.html"));
+    res.sendFile(path.join(__dirname, "public/index.html"));
   });
 
   // Load quiz page
   app.get("/quiz", function(req, res) {
+    //do we need the findAll?
     db.Questions.findAll({}).then(function(dbQuestions) {
-      res.sendFile(path.join(__dirname, "../views/index.html"));
+      res.sendFile(path.join(__dirname, "public/index.html"));
     });
   });
 
   // Load leaderboard page
   app.get("/leaderboard", function(req, res) {
     db.Leaderboard.findAll({}).then(function(dbLeaderbord) {
-      res.sendFile(path.join(__dirname, "../views/index.html"));
+      res.sendFile(path.join(__dirname, "public/index.html"));
     });
   });
 
-  app.get("/public/js/app.js", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/js/app.js"));
-  });
+  //Alternate routes?
+  // app.get("/public/js/app.js", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "public/js/app.js"));
+  // });
 
-  app.get("/index.css", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/index.css"));
-  });
+  // app.get("/index.css", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "public/index.css"));
+  // });
+
   // Load example page and pass in an example by id
   // app.get("/example/:id", function(req, res) {
   //   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
@@ -38,7 +41,7 @@ module.exports = function(app) {
   // });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
+  // app.get("*", function(req, res) {
+  //   res.render("404");
+  // });
 };
