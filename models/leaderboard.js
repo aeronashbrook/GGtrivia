6a4827ecf0140f47ab65aac1c39612416e143003
Spencer Caretta <spencer.caretta@gmail.com>
2019-01-18 17:02:04 -0600

    added a submit.js file for submit form

--- models/leaderboard.js
+++ models/leaderboard.js
@@ -1,23 +1,23 @@
-module.exports = function(sequelize, DataTypes) {
-    var Leaderboard = sequelize.define("leaderboard", {
-        name: {
-            type: DataTypes.STRING,
-            validate: {
-                isAlphanumeric: true,
-                allowNull: false,
-                len: [1, 50]
+module.exports = function (sequelize, DataTypes) {
+  var Leaderboard = sequelize.define("leaderboard", {
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        isAlphanumeric: true,
+        allowNull: false,
+        len: [1, 50]
 
-            }
-        },
-        score: {
-            type: DataTypes.INTEGER,
-            validate: {
-                allowNull: false,
-            }
-        },
-        order: [
-            ['score', 'DESC']
-        ]
-    });
-    return Leaderboard;
-}
+      }
+    },
+    score: {
+      type: DataTypes.INTEGER,
+      validate: {
+        allowNull: false,
+      }
+    },
+    order: [
+      ["score", "DESC"]
+    ]
+  });
+  return Leaderboard;
+};
---
+++ models/submit.js
@@ -0,0 +1,45 @@
+module.exports = function(sequelize, DataTypes) {
+  var Submit = sequelize.define("submitted", {
+    question: {
+      type: DataTypes.STRING,
+      validate: {
+        isAlphanumeric: true,
+        allowNull: false,
+        len: [1, 500]
+      }
+    },
+    correctAnswer: {
+      type: DataTypes.STRING,
+      validate: {
+        isAlphanumeric: true,
+        allowNull: false,
+        len: [1,255]
+      }
+    },
+    option1: {
+      type: DataTypes.STRING,
+      validate: {
+        isAlphanumeric: true,
+        allowNull: false,
+        len: [1,255]
+      }
+    },
+    option2: {
+      type: DataTypes.STRING,
+      validate: {
+        isAlphanumeric: true,
+        allowNull: false,
+        len: [1,255]
+      }
+    },
+    option3: {
+      type: DataTypes.STRING,
+      validate: {
+        isAlphanumeric: true,
+        allowNull: false,
+        len: [1,255]
+      }
+    }
+  });
+  return Submit;
+};
